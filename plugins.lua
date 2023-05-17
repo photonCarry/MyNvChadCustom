41d33cc2a3b1ae31afe2354a40e2afa712f82c0d
zuohufa <zuohufa@pinduoduo.com>
2023-05-17 17:09:54 +0800

    增加命令行搜索插件

--- plugins.lua
+++ plugins.lua
@@ -72,6 +72,43 @@ local plugins = {
       require("leap").add_default_mappings()
     end,
   },
+  {
+    "gelguy/wilder.nvim", -- 命令行搜索
+    dependencies = "romgrk/fzy-lua-native",
+    lazy = false,
+    config = function(_, opts)
+      -- http://neovimcraft.com/plugin/gelguy/wilder.nvim/index.html
+      local wilder = require("wilder")
+      wilder.setup({modes = {':', '/', '?'}})
+      -- Disable Python remote plugin
+      wilder.set_option('use_python_remote_plugin', 0)
+      wilder.set_option('pipeline', {
+        wilder.branch(
+          wilder.cmdline_pipeline({
+            fuzzy = 1,
+            fuzzy_filter = wilder.lua_fzy_filter(),
+          }),
+          wilder.vim_search_pipeline()
+        )
+      })
+      wilder.set_option('renderer', wilder.renderer_mux({
+        [':'] = wilder.popupmenu_renderer({
+          highlighter = wilder.lua_fzy_highlighter(),
+          left = {
+            ' ',
+            wilder.popupmenu_devicons()
+          },
+          right = {
+            ' ',
+            wilder.popupmenu_scrollbar()
+          },
+        }),
+        ['/'] = wilder.wildmenu_renderer({
+          highlighter = wilder.lua_fzy_highlighter(),
+        }),
+      }))
+    end,
+  },
   
 
   -- To make a plugin not be loaded
