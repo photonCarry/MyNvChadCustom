b9f6d223e85c6d5b7cc43bcb88e50a47f74d3ffa
zuohufa <zuohufa@pinduoduo.com>
2023-07-13 02:08:44 +0800

    设置背景透明

--- chadrc.lua
+++ chadrc.lua
@@ -7,6 +7,7 @@ local highlights = require "custom.highlights"
 M.ui = {
   theme = "onedark",
   theme_toggle = { "onedark", "one_light" },
+  transparency = true,
 
   hl_override = highlights.override,
   hl_add = highlights.add,
@@ -18,6 +19,14 @@ M.ui = {
     load_on_startup = true,
   },
   telescope = { style = "bordered" },
+
+  statusline = {
+    theme = "vscode_colored", -- default/vscode/vscode_colored/minimal
+    -- default/round/block/arrow separators work only for default statusline theme
+    -- round and block will work for minimal theme only
+    separator_style = "default",
+    overriden_modules = nil,
+  },
 }
 
 M.plugins = "custom.plugins"
