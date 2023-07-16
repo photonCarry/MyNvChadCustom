603622fa3b4dc7e24aac1224aabcefe9c5fb14b2
zuohufa <zuohufa@pinduoduo.com>
2023-07-16 21:57:31 +0800

    自动恢复上次编辑位置

--- plugins.lua
+++ plugins.lua
@@ -157,7 +157,19 @@ local plugins = {
         skip_multiline = true,
     }
     end,
-  }
+  },
+  -- 恢复上次编辑位置
+  {
+    "ethanholz/nvim-lastplace",
+    lazy = false,
+    init = function()
+      require("nvim-lastplace").setup {
+        lastplace_ignore_buftype = {"quickfix", "nofile", "help"},
+        lastplace_ignore_filetype = {"gitcommit", "gitrebase", "svn", "hgcommit"},
+        lastplace_open_folds = true
+      }
+    end,
+  },
   
 
   -- To make a plugin not be loaded
