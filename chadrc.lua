6afc1fc57e6778f3fbcd138557bd924348d41970
zuohufa <zuohufa@pinduoduo.com>
2023-07-21 17:50:27 +0800

    换主题

--- chadrc.lua
+++ chadrc.lua
@@ -5,9 +5,8 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "onedark",
-  theme_toggle = { "onedark", "one_light" },
-  transparency = true,
+  theme = "monekai",
+  transparency = false,
 
   hl_override = highlights.override,
   hl_add = highlights.add,
